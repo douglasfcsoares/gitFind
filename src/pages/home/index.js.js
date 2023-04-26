ff74ecd48bcf8ce90370fc9b35c5e525025c3c0e
douglasfcsoares <douglasfcsoares@gmail.com>
2023-04-25 23:40:51 -0300

    adicionando input e button

--- src/pages/home/index.js.js
+++ src/pages/home/index.js.js
@@ -1,9 +1,21 @@
 import { Header } from "../../components/header/index.js"
+import background from "../../assets/octocat.png"
+import './styles.css'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className="content">
+        <img src={background} className="background" alt="background app" />
+        <div className="info">
+          <p>Informações</p>
+          <div>
+            <input name="user" placeholder="@username" />
+            <button>Search</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
