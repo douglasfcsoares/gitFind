1304694051a603a20ffdf21917162ebad05857f4
douglasfcsoares <douglasfcsoares@gmail.com>
2023-05-04 22:25:57 -0300

    Crtiação do componente item-list

--- src/pages/home/index.js.js
+++ src/pages/home/index.js.js
@@ -1,6 +1,7 @@
-import { Header } from "../../components/header/index.js"
-import background from "../../assets/octocat.png"
-import './styles.css'
+import { Header } from "../../components/header/index.js";
+import ItemList, {} from "../../components/itemList/index.jsx";
+import background from "../../assets/octocat.png";
+import './styles.css';
 
 function App() {
   return (
@@ -9,10 +10,26 @@ function App() {
       <div className="content">
         <img src={background} className="background" alt="background app" />
         <div className="info">
-          <p>Informações</p>
           <div>
             <input name="user" placeholder="@username" />
             <button>Search</button>
+          </div>
+          <div className="perfil">
+            <img src="https://avatars.githubusercontent.com/u/62715161?s=400&u=7351297a7a413114cc424c3bba2a3407a084dae9&v=4" className="profile" alt="Imagem do perfil do Dev" />
+            <div>
+              <h3>
+                Douglas Soares
+              </h3>
+              <span>@douglasfcsoares</span>
+              <p>Descrição do perfil</p>
+            </div>
+          </div>
+          <hr />
+          <div className="repository">
+            <h4>Repositórios</h4>
+            <ItemList title='Teste1' description='teste de descrição 01' />
+            <ItemList title='Teste2' description='teste de descrição 02' />
+            <ItemList title='Teste3' description='teste de descrição 03' />
           </div>
         </div>
       </div>
